0b75114d613db664976c7d12738513e742365ed9
zhuwe <zhuwe@microsoft.com>
2020-02-14 00:32:42 +0800

    read blobof

---
+++ code/pre/read_BlobofText.py
@@ -0,0 +1,27 @@
+import sys
+import json
+import os
+
+pageset=set()
+max_l=0
+
+def read_one(filename):
+    global max_l
+    with open(filename,'r',encoding='utf-8') as f:
+        jobj=json.load(f)
+        text_buffer=[]
+        for page in jobj:
+            text_buffer.append(jobj[page])
+            pageset.add(page)
+        text_buffer='\n'.join(text_buffer)
+        max_l=max(max_l,len(text_buffer.split()))
+    return text_buffer
+
+def read_all(input_dir):
+    files=os.listdir(input_dir)
+    for file in files:
+        file=os.listdir(os.path.join(file,'AggregatedBlobofText'))[0]
+        read_one(file)
+
+
+read_all(sys.argv[1])
